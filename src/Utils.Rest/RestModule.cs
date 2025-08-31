23e77941457153450ad2f9b4c644a2e675797da0
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2025-08-31 15:05:54 +0200

    chore: Setup repository (#2)
    Author: Felix Hake

---
+++ src/Utils.Rest/RestModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Converters;
+using Utils.Core.Infrastructure.Helpers;
+
+namespace Utils.Rest;
+
+public class RestModule(IAssemblyHelper assemblyHelper) : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        var collection = new ServiceCollection();
+
+        var mvcBuilder = collection.AddControllers();
+        foreach (var assembly in assemblyHelper.GetAllAssemblies())
+        {
+            mvcBuilder.AddApplicationPart(assembly);
+        }
+        mvcBuilder.AddNewtonsoftJson(options =>
+        {
+            options.AllowInputFormatterExceptionMessages = true;
+            options.ReadJsonWithRequestCulture = true;
+            options.SerializerSettings.Converters.Add(new StringEnumConverter());
+        });
+
+        builder.Populate(collection);
+    }
+}
